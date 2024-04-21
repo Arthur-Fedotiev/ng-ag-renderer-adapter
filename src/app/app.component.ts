cd741e870cd7ebef11e01d07beed458df53855fb
arturfedotiev <artur.fedotiew@gmail.com>
2024-04-21 07:22:55 +0200

    code: Setup base project with ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,67 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ngagra-app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <ag-grid-angular
+      style="width: 100%; height: 500px;"
+      class="ag-theme-alpine"
+      [gridOptions]="gridOptions"
+      [columnDefs]="columnDefs"
+      [rowData]="rowData"
+    >
+    </ag-grid-angular>
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'ng-ag-renderer-adapter';
+  protected readonly gridOptions = {
+    defaultColDef: {
+      cellRenderer: 'heavyCellRenderer',
+      cellRendererParams: {
+        // params for your heavy cell renderer
+      },
+    },
+  };
+
+  // Define column definitions
+  protected readonly columnDefs = [
+    { field: 'fundName' },
+    { field: 'ticker' },
+    { field: 'assetClass' },
+    { field: 'totalAssets' },
+    { field: 'yield' },
+    { field: 'price' },
+    { field: 'fundFlows' },
+    { field: 'returnYTD' },
+    { field: 'return1Year' },
+    { field: 'return3Year' },
+  ];
+
+  // Define row data
+  protected readonly rowData = [
+    // Add your row data here
+  ];
+
+  createMockData(count: number) {
+    const data = [];
+    for (let i = 0; i < count; i++) {
+      data.push({
+        fundName: `Fund ${i}`,
+        ticker: `Ticker ${i}`,
+        assetClass: `Asset Class ${i}`,
+        totalAssets: `Total Assets ${i}`,
+        yield: `Yield ${i}`,
+        price: `Price ${i}`,
+        fundFlows: `Fund Flows ${i}`,
+        returnYTD: `Return YTD ${i}`,
+        return1Year: `Return 1 Year ${i}`,
+        return3Year: `Return 3 Year ${i}`,
+      });
+    }
+    return data;
+  }
 }
